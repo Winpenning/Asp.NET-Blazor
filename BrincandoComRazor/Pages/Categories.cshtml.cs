0776e69829d9d3c4d4db9511697202f1a1d43038
Paulo Henrique Ziemer dos Santos <zs.phenriqueprofessional@gmail.com>
2024-05-08 16:22:50 -0300

    Razor pages

--- BrincandoComRazor/Pages/Categories.cshtml.cs
+++ BrincandoComRazor/Pages/Categories.cshtml.cs
@@ -1,11 +1,18 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using BrincandoComRazor.Data;
+using BrincandoComRazor.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BrincandoComRazor.Pages;
 
 public class Categories : PageModel
 {
-    public void OnGet()
+    public List<Category> categories=new();
+    public async Task OnGet()
     {
-        
+        var context = new AppDbContext();
+        categories = context.Categories.ToList();
     }
 }
