bfe9d4424ce9a955c3d1ad4dcf7e4f24452e5878
Paulo Henrique Ziemer dos Santos <zs.phenriqueprofessional@gmail.com>
2024-05-10 09:05:42 -0300

    Menu de navegação atualizado

--- BlazingShop/obj/Debug/net7.0/BlazingShop.AssemblyInfo.cs
+++ BlazingShop/obj/Debug/net7.0/BlazingShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazingShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7920c4aeb8c78151a666670c1c897f69d31f1d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6b60a90244d1cc6813e2361fb5c9d92b8b93197")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazingShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazingShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
