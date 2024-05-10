7c40e383081f5ed2f439293168d66b41ad03ae83
Paulo Henrique Ziemer dos Santos <zs.phenriqueprofessional@gmail.com>
2024-05-10 09:51:04 -0300

    Primeiro protótipo da página de criação de categorias

--- BlazingShop/obj/Debug/net7.0/BlazingShop.AssemblyInfo.cs
+++ BlazingShop/obj/Debug/net7.0/BlazingShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazingShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6b60a90244d1cc6813e2361fb5c9d92b8b93197")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f36f84a818c31be7fab82f60fddbcf565e50f4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazingShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazingShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
